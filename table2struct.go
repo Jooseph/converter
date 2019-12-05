443ee5a59164ce14b18026c2e3640e4c4a42d81b
Siu_Joseph <1092113110@qq.com>
2019-12-05 11:58:46 +0800

    add separate file option

--- table2struct.go
+++ table2struct.go
@@ -154,49 +154,60 @@ func (t *Table2Struct) Run() error {
 	}
 
 	// 组装struct
+	if t.config.SeparateFile {
+		return t.writeSeparateFile(tableColumns, packageName)
+	} else {
+		return t.writeSingleFile(tableColumns, packageName)
+	}
+}
+
+func (t *Table2Struct) writeSeparateFile(tableColumns map[string][]column, packageName string) error {
+	structContentes := make(map[string]string)
+	for tableRealName, item := range tableColumns {
+		structContent := t.makeStructContent(tableRealName, item)
+		// 如果有引入 time.Time, 则需要引入 time 包
+		var importContent string
+		if strings.Contains(structContent, "time.Time") {
+			importContent = "import \"time\"\n\n"
+		}
+		structContentes[tableRealName] = packageName + importContent + structContent
+	}
+
+	// 写入文件struct
+	var savePath = t.savePath
+	// 是否指定保存路径
+	if savePath == "" {
+		savePath = "model.go"
+	}
+
+	for fileName, fileContent := range structContentes {
+		filePath := fmt.Sprintf("%s/%s.go", savePath, fileName)
+		writeFile(filePath, fileContent)
+	}
+
+	return nil
+}
+
+func writeFile(filePath string, content string) error {
+	f, err := os.Create(filePath)
+	if err != nil {
+		fmt.Println("Can not write file")
+		return err
+	}
+	defer f.Close()
+
+	f.WriteString(content)
+
+	cmd := exec.Command("gofmt", "-w", filePath)
+	cmd.Run()
+
+	return nil
+}
+
+func (t *Table2Struct) writeSingleFile(tableColumns map[string][]column, packageName string) error {
 	var structContent string
 	for tableRealName, item := range tableColumns {
-		// 去除前缀
-		if t.prefix != "" {
-			tableRealName = tableRealName[len(t.prefix):]
-		}
-		tableName := tableRealName
-		structName := tableName
-		if t.config.StructNameToHump {
-			structName = t.camelCase(structName)
-		}
-
-		switch len(tableName) {
-		case 0:
-		case 1:
-			tableName = strings.ToUpper(tableName[0:1])
-		default:
-			// 字符长度大于1时
-			tableName = strings.ToUpper(tableName[0:1]) + tableName[1:]
-		}
-		depth := 1
-		structContent += "type " + structName + " struct {\n"
-		for _, v := range item {
-			//structContent += tab(depth) + v.ColumnName + " " + v.Type + " " + v.Json + "\n"
-			// 字段注释
-			var clumnComment string
-			if v.ColumnComment != "" {
-				clumnComment = fmt.Sprintf(" // %s", v.ColumnComment)
-			}
-			structContent += fmt.Sprintf("%s%s %s %s%s\n",
-				tab(depth), v.ColumnName, v.Type, v.Tag, clumnComment)
-		}
-		structContent += tab(depth-1) + "}\n\n"
-
-		// 添加 method 获取真实表名
-		if t.realNameMethod != "" {
-			structContent += fmt.Sprintf("func (*%s) %s() string {\n",
-				structName, t.realNameMethod)
-			structContent += fmt.Sprintf("%sreturn \"%s\"\n",
-				tab(depth), tableRealName)
-			structContent += "}\n\n"
-		}
-		fmt.Println(structContent)
+		structContent = t.makeStructContent(tableRealName, item)
 	}
 
 	// 如果有引入 time.Time, 则需要引入 time 包
@@ -212,19 +223,54 @@ func (t *Table2Struct) Run() error {
 		savePath = "model.go"
 	}
 	filePath := fmt.Sprintf("%s", savePath)
-	f, err := os.Create(filePath)
-	if err != nil {
-		fmt.Println("Can not write file")
-		return err
+	fileContent := packageName + importContent + structContent
+	return writeFile(filePath, fileContent)
+}
+
+func (t *Table2Struct) makeStructContent(tableRealName string, columns []column) string {
+	var structContent string
+	// 去除前缀
+	if t.prefix != "" {
+		tableRealName = tableRealName[len(t.prefix):]
 	}
-	defer f.Close()
+	tableName := tableRealName
+	structName := tableName
+	if t.config.StructNameToHump {
+		structName = t.camelCase(structName)
+	}
 
-	f.WriteString(packageName + importContent + structContent)
+	switch len(tableName) {
+	case 0:
+	case 1:
+		tableName = strings.ToUpper(tableName[0:1])
+	default:
+		// 字符长度大于1时
+		tableName = strings.ToUpper(tableName[0:1]) + tableName[1:]
+	}
+	depth := 1
+	structContent += "type " + structName + " struct {\n"
+	for _, v := range columns {
+		//structContent += tab(depth) + v.ColumnName + " " + v.Type + " " + v.Json + "\n"
+		// 字段注释
+		var columnComment string
+		if v.ColumnComment != "" {
+			columnComment = fmt.Sprintf(" // %s", v.ColumnComment)
+		}
+		structContent += fmt.Sprintf("%s%s %s %s%s\n",
+			tab(depth), v.ColumnName, v.Type, v.Tag, columnComment)
+	}
+	structContent += tab(depth-1) + "}\n\n"
 
-	cmd := exec.Command("gofmt", "-w", filePath)
-	cmd.Run()
-
-	return nil
+	// 添加 method 获取真实表名
+	if t.realNameMethod != "" {
+		structContent += fmt.Sprintf("func (*%s) %s() string {\n",
+			structName, t.realNameMethod)
+		structContent += fmt.Sprintf("%sreturn \"%s\"\n",
+			tab(depth), tableRealName)
+		structContent += "}\n\n"
+	}
+	fmt.Println(structContent)
+	return structContent
 }
 
 func (t *Table2Struct) dialMysql() {
